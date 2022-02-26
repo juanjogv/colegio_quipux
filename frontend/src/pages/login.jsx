cd12d32e2dbaf0c55500bc77ebe060a2c0249346
Juan Jose Garcia <juangarciavxx@gmail.com>
2022-02-26 15:31:48 -0500

    agregando bootstrap y haciendo unos ajustes al formulario de logeo

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,38 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import '../style/login.css';
-
+//import '../style/login.css';
 
 const Login = () => {
-  
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Hola mundo")
-  }
+    console.log("Hola mundo");
+  };
 
-  return(
-    <>
-      <form onSubmit={handleSubmit}>
-                <div className="container">
-                    <input
-                        type="text"
-                        placeholder="Enter Username"
-                        name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)} required />
-                    <input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="psw"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)} required />
-                    <button type="submit">Login</button>
-                </div>
-        </form>
-    </>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col">
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <div id="emailHelp" class="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">
+                Password
+              </label>
+              <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
